9d1c0189e951b7c1dd4d66af0089af9f3e5cc6aa
حسن الخليف <roh.baba.11@gmail.com>
2023-10-29 23:04:10 +0300

    create my text form widget

--- lib/widget/my_text_form_field.dart
+++ lib/widget/my_text_form_field.dart
@@ -3,18 +3,22 @@ import 'package:flutter/material.dart';
 class MyTextFormField extends StatelessWidget {
   const MyTextFormField({
     super.key,
-    required this.controller, required this.hint, required this.lable,
+    required this.controller,
+    required this.hint,
+    required this.lable,
+    this.autofocus = false,
   });
 
   final TextEditingController controller;
   final String hint;
   final String lable;
+  final bool autofocus;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
-      autofocus: true,
+      autofocus: autofocus,
       decoration: InputDecoration(
           border: const OutlineInputBorder(),
           label: Text(
