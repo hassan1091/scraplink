71dab0702e10623d0678409048cf195c55c2c79f
حسن الخليف <roh.baba.11@gmail.com>
2023-10-31 09:26:36 +0300

    finish register page

---
+++ lib/page/register.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:scraplink/widget/my_text_form_field.dart';
+
+class RegisterPage extends StatelessWidget {
+  RegisterPage({super.key});
+
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final phoneController = TextEditingController();
+  final cityController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(children: [
+            const SizedBox(height: 6),
+            MyTextFormField(
+                controller: nameController, hint: "Name", lable: "Name",autofocus: true,),
+            const SizedBox(height: 12),
+            MyTextFormField(
+                controller: emailController, hint: "Email", lable: "Email"),
+            const SizedBox(height: 12),
+            MyTextFormField(
+                controller: passwordController,
+                hint: "Password",
+                lable: "Password"),
+            const SizedBox(height: 12),
+            MyTextFormField(
+                controller: phoneController, hint: "Phone", lable: "Phone"),
+            const SizedBox(height: 12),
+            MyTextFormField(
+                controller: cityController, hint: "City", lable: "City"),
+            const SizedBox(height: 12),
+            ElevatedButton(
+                onPressed: () {}, child: const Text("Create Account"))
+          ]),
+        ),
+      ),
+    );
+  }
+}
