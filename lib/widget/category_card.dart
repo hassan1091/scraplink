8ea67ed31169fba4163a45cd2371d42b30cf4800
حسن الخليف <roh.baba.11@gmail.com>
2023-10-31 14:58:49 +0300

    add car brand logos

---
+++ lib/widget/category_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({super.key, required this.name});
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: const BoxDecoration(
+            boxShadow: [BoxShadow(blurRadius: 4, color: Colors.black)],
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xFFE97171), Color(0xFFFFCB8E)])),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+              width: 150,
+              child: Image(
+                  image: AssetImage("assets/images/${name}_logo.png"),
+                  fit: BoxFit.contain),
+            ),
+          ],
+        ),
+      ),
+      onTap: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+}
