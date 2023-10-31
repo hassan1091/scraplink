cd51299e9e567c31c5a3e63a6676c96b5407199c
حسن الخليف <roh.baba.11@gmail.com>
2023-10-31 15:01:23 +0300

    add primary color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'page/login.dart';
 
 void main() {
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ScrapLink',
       theme: ThemeData(
+        primaryColor: const Color(0xff810000),
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff810000)),
       ),
       home: const LoginPage(),
