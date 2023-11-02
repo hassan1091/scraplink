d8ba901d18a166e0dd3847b541352692d99bf7cb
حسن الخليف <roh.baba.11@gmail.com>
2023-11-03 01:49:36 +0300

    implement the functionality of contact button

--- lib/page/home/map_widget.dart
+++ lib/page/home/map_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scraplink/widget/yard_card.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MapWidget extends StatefulWidget {
   const MapWidget({super.key});
@@ -10,6 +11,19 @@ class MapWidget extends StatefulWidget {
 
 class _MapWidgetState extends State<MapWidget> {
   String? selectedCity;
+
+  void contact(phoneNumber) {
+    Uri url = Uri.parse("https://wa.me/$phoneNumber");
+    launchUrl(url, mode: LaunchMode.externalApplication).then((value) {
+      if (!value) {
+        showDialog(
+            context: context,
+            builder: (_) => const AlertDialog(
+                title: Text(
+                    "Could not launch WhatsApp. Make sure WhatsApp is installed on your device.")));
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,12 +59,28 @@ class _MapWidgetState extends State<MapWidget> {
         Expanded(
           child: GridView.count(
             crossAxisCount: 2,
-            children: const [
-              YardCard(name: "scrap 1", city: "Al-damam"),
-              YardCard(name: "scrap 2", city: "mam"),
-              YardCard(name: "scrap 3", city: "mam"),
-              YardCard(name: "scrap 4", city: "mam"),
-              YardCard(name: "scrap 55", city: "mam"),
+            children: [
+              YardCard(
+                name: "scrap 1",
+                city: "Al-damam",
+                onPressed: () => contact("+966502130371"),
+              ),
+              YardCard(
+                  name: "scrap 2",
+                  city: "mam",
+                  onPressed: () => contact("+9665021371")),
+              YardCard(
+                  name: "scrap 3",
+                  city: "mam",
+                  onPressed: () => contact("+9665021371")),
+              YardCard(
+                  name: "scrap 4",
+                  city: "mam",
+                  onPressed: () => contact("+9665021371")),
+              YardCard(
+                  name: "scrap 55",
+                  city: "mam",
+                  onPressed: () => contact("+9665021371")),
             ],
           ),
         )
