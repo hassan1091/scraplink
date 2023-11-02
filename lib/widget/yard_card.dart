bcbb187281837c684750a2fbbd752ee93e38ac02
حسن الخليف <roh.baba.11@gmail.com>
2023-11-03 01:08:25 +0300

    create yard card

---
+++ lib/widget/yard_card.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class YardCard extends StatelessWidget {
+  const YardCard({
+    super.key,
+    required this.name,
+    required this.city,
+    this.onPressed,
+  });
+
+  final String name;
+  final String city;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          ClipRRect(
+            borderRadius:
+                const BorderRadius.vertical(top: Radius.circular(16.0)),
+            child: Image.network(
+              "https://static.thenounproject.com/png/46960-200.png",
+              height: 100,
+              fit: BoxFit.contain,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Name: $name",
+                  style: theme.textTheme.titleLarge,
+                ),
+                Text(
+                  "City: $city",
+                  style: theme.textTheme.bodyMedium,
+                ),
+                ElevatedButton(
+                  onPressed: onPressed,
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateColor.resolveWith(
+                          (states) => Colors.green)),
+                  child: const Row(
+                    children: [
+                      Image(
+                        height: 25,
+                        width: 25,
+                        color: Colors.white,
+                        image: AssetImage("assets/icons/whatsapp.png"),
+                      ),
+                      // SizedBox(width: 4),
+                      Text(
+                        "Contact",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
