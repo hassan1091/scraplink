df8065373994ff9180d5c461cca4abd49ef32cb3
حسن الخليف <roh.baba.11@gmail.com>
2023-11-04 11:04:39 +0300

    create car card and add it to profile widget

--- lib/page/home/profile_widget.dart
+++ lib/page/home/profile_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scraplink/my_theme.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -7,19 +8,89 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       padding: const EdgeInsets.all(16),
-      children: const [
-        _ProfileHeader(),
-        SizedBox(
+      children: [
+        const _ProfileHeader(),
+        const SizedBox(
           height: 4,
         ),
-        Divider(
+        const Divider(
           color: Colors.black,
           height: 4,
         ),
-        SizedBox(
+        const SizedBox(
           height: 4,
         ),
+        _CarCard(
+          "Toyota Camry",
+          2013,
+          "https://www.copart.com/content/us/en/images/landing-pages/FrontEnd.jpg",
+          () {},
+        ),
+        _CarCard(
+          "Toyota Camry",
+          2013,
+          "https://www.copart.com/content/us/en/images/landing-pages/FrontEnd.jpg",
+          () {},
+        ),
+        _CarCard(
+          "Toyota Camry",
+          2013,
+          "https://www.copart.com/content/us/en/images/landing-pages/FrontEnd.jpg",
+          () {},
+        ),
       ],
+    );
+  }
+}
+
+class _CarCard extends StatelessWidget {
+  const _CarCard(this.name, this.year, this.image, this.onPressed);
+
+  final String name;
+  final int year;
+  final String image;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Row(
+          children: [
+            const Image(
+              image: NetworkImage(
+                  "https://www.copart.com/content/us/en/images/landing-pages/FrontEnd.jpg"),
+              height: 116,
+              width: 124,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: MyTheme().titleStyle,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  year.toString(),
+                  style: MyTheme().subtitleStyle,
+                )
+              ],
+            ),
+          ],
+        ),
+        FloatingActionButton(
+          onPressed: onPressed,
+          child: const Icon(Icons.arrow_forward_ios),
+        ),
+        const SizedBox()
+      ]),
     );
   }
 }
@@ -42,17 +113,14 @@ class _ProfileHeader extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "rayan hassan",
-              style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
+              "Ali hassan",
+              style: MyTheme().titleStyle,
             ),
             ElevatedButton(
                 onPressed: () {},
-                child: const Text(
+                child: Text(
                   "Edit profile",
-                  style: TextStyle(fontSize: 20),
+                  style: MyTheme().buttonTextStyle,
                 ))
           ],
         )
