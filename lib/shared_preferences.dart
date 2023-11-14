d45ca302e659f0d264fe2c1808e1616591194ac2
حسن الخليف <roh.baba.11@gmail.com>
2023-11-14 10:29:31 +0300

    create app local storage

---
+++ lib/shared_preferences.dart
@@ -0,0 +1,24 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum AppStorageKey { id, role }
+
+class AppLocalStorage {
+  static SharedPreferences? _prefs;
+
+  static Future<SharedPreferences> get prefs async {
+    _prefs ??= await SharedPreferences.getInstance();
+    return _prefs!;
+  }
+
+  static Future<void> setString(AppStorageKey key, String value) async {
+    (await prefs).setString(key.toString(), value);
+  }
+
+  static Future<String?> getString(AppStorageKey key) async {
+    return (await prefs).getString(key.toString());
+  }
+
+  static Future<void> delete(AppStorageKey key) async {
+    (await prefs).remove(key.toString());
+  }
+}
