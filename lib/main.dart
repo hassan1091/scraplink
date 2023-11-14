42b2caf792435122f771feb6ef6c1fbaf769323a
حسن الخليف <roh.baba.11@gmail.com>
2023-11-14 10:56:30 +0300

    init supabase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:scraplink/my_theme.dart';
 import 'package:scraplink/page/login.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    url: "https://xpfrdvwfquqhzzijsboi.supabase.co",
+    anonKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwZnJkdndmcXVxaHp6aWpzYm9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkxMTE0ODUsImV4cCI6MjAxNDY4NzQ4NX0.jmXg5tfmI1k_RErtRQyci_upcHLIh5RCs3ShhtksfZs",
+  );
   runApp(const MyApp());
 }
 
