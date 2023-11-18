efa8c929acf660edfef43587807da950ef6902f5
حسن الخليف <roh.baba.11@gmail.com>
2023-11-18 13:49:56 +0300

    implement sell car and connect it to database

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:scraplink/api/model/car.dart';
 import 'package:scraplink/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -26,12 +29,26 @@ class ApiService {
         Individual.fromJson(response).individualId.toString());
   }
 
-  Future<void> sellCar(Car car) async {
+  Future<void> sellCar(Car car, String imagePath) async {
     car.individualId =
         int.parse((await AppLocalStorage.getString(AppStorageKey.id))!);
-    final response = await Supabase.instance.client
+    car.imageUrl = (await _uploadImage(imagePath));
+    await Supabase.instance.client
         .from('individual_salvage_car')
         .insert(car.toJson());
-    if (response == null) throw Exception("Failed to add car");
   }
+
+  Future<String> _uploadImage(String imagePath, {bool isPart = false}) async {
+    String imageUrl = _generateUniqueFileName(imagePath);
+    await Supabase.instance.client.storage
+        .from(isPart ? "part" : "car")
+        .upload(imageUrl, File(imagePath));
+    return Supabase.instance.client.storage
+        .from(isPart ? "part" : "car")
+        .getPublicUrl(imageUrl);
+  }
+
+  String _generateUniqueFileName(String imagePath) =>
+      '${DateTime.now().toUtc().toIso8601String()}-${List.generate(6, (_) => Random().nextInt(36).toString()).join()}-${imagePath.split('/').last.replaceAll(RegExp(r'[^\w\s\-.]'), '_')}'
+          .replaceAll(" ", "");
 }
