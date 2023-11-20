fe7f80dc14058f02429ada1ac7338ed6d3548354
حسن الخليف <roh.baba.11@gmail.com>
2023-11-20 11:18:32 +0300

    create constants class

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+class Constants {
+  static const List<String> regions = [
+    'Riyadh',
+    'Makkah',
+    'Madinah',
+    'Eastern Province',
+    'Asir',
+    'Tabuk',
+    'Hail',
+    'Northern Borders',
+    'Jazan',
+    'Najran',
+    'Al Bahah',
+    'Al Jawf',
+    'Qassim'
+  ];
+}
