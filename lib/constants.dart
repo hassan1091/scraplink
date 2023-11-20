420ee0ed28735342e964cc316b0bd85784128b48
حسن الخليف <roh.baba.11@gmail.com>
2023-11-20 17:47:02 +0300

    create models and partCategory static data

--- lib/constants.dart
+++ lib/constants.dart
@@ -14,4 +14,85 @@ class Constants {
     'Al Jawf',
     'Qassim'
   ];
+
+  static const Map<String, List<String>> models = {
+    "Toyota": [
+      "Corolla",
+      "Avalon",
+      "Camry",
+      "FJCruiser",
+      "Hilux",
+      "Innova",
+      "LandCruiser",
+      "RAV4",
+      "Yaris",
+      "Avanza",
+      "Rush",
+      "Highlander",
+      "LandCruiser_PickUp",
+      "Prado",
+    ],
+    "Ford": [
+      "Taurus",
+      "Expedition",
+      "Explorer",
+      "F-150",
+      "Figo",
+      "Edge",
+      "CrownVictoria",
+      "Ranger",
+      "Figo",
+      "Territory",
+    ],
+    "Honda": [
+      "Civic",
+      "Accord",
+      "CR-V",
+      "Pilot",
+      "Odyssey",
+      "City",
+      "HR-V",
+    ],
+    "Hyundai": [
+      "Sonata",
+      "Kona",
+      "Accent",
+      "Azera",
+      "Elantra",
+      "SantaFe",
+      "Palisade",
+      "Veloster",
+      "Creta",
+      "Tucson",
+    ],
+    "Kia": [
+      "Cadenza",
+      "Cerato",
+      "Rio",
+      "K5",
+      "Carnival",
+      "Pegas",
+      "Sorento",
+      "Telluride",
+      "K8",
+      "Sportage",
+    ],
+    "Jeep": [
+      "Cherokee",
+      "Compass",
+      "GrandCherokee",
+      "Wrangler",
+      "Gladiator",
+    ]
+  };
+
+  static const List<String> partCategory = [
+    "Engine  Components",
+    "Drivetrain",
+    "Suspension and Steering",
+    "Body and Frame",
+    "Interior Components",
+    "Wheels and Tires",
+    "Sensors and Modules"
+  ];
 }
