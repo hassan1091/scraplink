795389bdfe8075c7fb6516040dc5d7e489c59854
حسن الخليف <roh.baba.11@gmail.com>
2023-11-20 16:31:24 +0300

    edit my text form field

--- lib/widget/my_text_form_field.dart
+++ lib/widget/my_text_form_field.dart
@@ -1,30 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class MyTextFormField extends StatelessWidget {
-  const MyTextFormField({
-    super.key,
-    required this.controller,
-    required this.hint,
-    required this.lable,
-    this.autofocus = false,
-  });
-
-  final TextEditingController controller;
-  final String hint;
+class MyTextFormField extends StatefulWidget {
+  const MyTextFormField(
+      {Key? key,
+      required this.lable,
+      this.controller,
+      this.hint,
+      this.initial,
+      this.isPassword = false,
+      this.isReadOnly = false,
+      this.isAddressMap = false,
+      this.autofocus = false,
+      this.type,
+      this.validator,
+      this.onTap,
+      this.onEditingComplete})
+      : super(key: key);
   final String lable;
+  final TextEditingController? controller;
+  final String? hint;
+  final String? initial;
+  final TextInputType? type;
+  final FormFieldValidator<String>? validator;
+  final bool isPassword;
+  final bool isReadOnly;
+  final bool isAddressMap;
   final bool autofocus;
+  final VoidCallback? onTap;
+  final VoidCallback? onEditingComplete;
+
+  @override
+  State<MyTextFormField> createState() => _MyTextFormFieldState();
+}
+
+class _MyTextFormFieldState extends State<MyTextFormField> {
+  late bool isSecure;
+
+  void switchSecure() {
+    setState(() {
+      isSecure = !isSecure;
+    });
+  }
+
+  @override
+  void initState() {
+    isSecure = widget.isPassword;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: controller,
-      autofocus: autofocus,
       decoration: InputDecoration(
+          label: Text(widget.lable),
+          labelStyle: const TextStyle(fontWeight: FontWeight.bold),
           border: const OutlineInputBorder(),
-          label: Text(
-            lable,
-          ),
-          hintText: hint),
+          suffixIcon: widget.isPassword
+              ? IconButton(
+                  onPressed: switchSecure,
+                  icon:
+                      Icon(isSecure ? Icons.visibility_off : Icons.visibility))
+              : widget.isAddressMap
+                  ? IconButton(
+                      onPressed: widget.onTap,
+                      icon: const Icon(Icons.add_location_alt_outlined))
+                  : null,
+          hintText: widget.hint),
+      autofocus: widget.autofocus,
+      keyboardType: widget.type,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: widget.validator,
+      initialValue: widget.initial,
+      inputFormatters: widget.type == TextInputType.number
+          ? <TextInputFormatter>[
+              FilteringTextInputFormatter.allow(RegExp(r'\d')),
+            ]
+          : null,
+      controller: widget.controller,
+      obscureText: isSecure,
+      readOnly: widget.isReadOnly,
+      enableSuggestions: !isSecure,
+      onEditingComplete: widget.onEditingComplete,
+      onTap: !widget.isAddressMap ? widget.onTap : null,
     );
   }
 }
