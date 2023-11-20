c193c70f56cc4e59586267ac038595c95156f2a6
حسن الخليف <roh.baba.11@gmail.com>
2023-11-21 01:40:14 +0300

    add bids to car details

--- lib/page/home/car_details.dart
+++ lib/page/home/car_details.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:scraplink/api/api_service.dart';
+import 'package:scraplink/api/model/bid.dart';
 import 'package:scraplink/api/model/car.dart';
 import 'package:scraplink/my_theme.dart';
 
@@ -50,68 +52,80 @@ class CarDetailsPage extends StatelessWidget {
               ),
             ),
           ),
-          Expanded(
-            child: GridView.builder(
-              itemCount: 9,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, // Change as needed
-                childAspectRatio: MediaQuery.of(context).size.width /
-                    ((MediaQuery.of(context).size.height + 100) / 4),
-              ),
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  elevation: 10,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16.0),
+          FutureBuilder(
+            future: ApiService().getBids(car.carId),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              }
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+              List<Bid> bids = snapshot.data!;
+
+              return Expanded(
+                child: GridView.builder(
+                  itemCount: bids.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // Change as needed
+                    childAspectRatio: 1.5,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Expanded(
-                      child: Column(
-                        children: <Widget>[
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Expanded(
+                          child: Column(
                             children: <Widget>[
-                              Text(
-                                "Name: bidder1",
-                                style: MyTheme().titleStyle,
-                              ),
-                              const SizedBox(height: 4),
-                              Text(
-                                "price: 1420",
-                                style: MyTheme().subtitleStyle,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  IconButton(
-                                    color: Colors.green,
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Icons.check_circle_outline,
-                                      size: 50,
-                                    ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    bids[index].vendor!.name!,
+                                    style: MyTheme().titleStyle,
                                   ),
-                                  const SizedBox(width: 32),
-                                  IconButton(
-                                    color: Colors.red,
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      CupertinoIcons.xmark_circle,
-                                      size: 50,
-                                    ),
+                                  const SizedBox(height: 4),
+                                  Text(
+                                    "price: ${bids[index].price}",
+                                    style: MyTheme().subtitleStyle,
                                   ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      IconButton(
+                                        color: Colors.green,
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          Icons.check_circle_outline,
+                                          size: 50,
+                                        ),
+                                      ),
+                                      const SizedBox(width: 32),
+                                      IconButton(
+                                        color: Colors.red,
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          CupertinoIcons.xmark_circle,
+                                          size: 50,
+                                        ),
+                                      ),
+                                    ],
+                                  )
                                 ],
-                              )
+                              ),
                             ],
                           ),
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
-                );
-              },
-            ),
+                    );
+                  },
+                ),
+              );
+            },
           ),
         ],
       ),
