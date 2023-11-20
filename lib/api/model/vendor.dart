d8cb820d10f78312f2cf7f5ed449d08ac2312cf0
حسن الخليف <roh.baba.11@gmail.com>
2023-11-20 13:08:22 +0300

    create vendor class

---
+++ lib/api/model/vendor.dart
@@ -0,0 +1,38 @@
+class Vendor {
+  int? id;
+  DateTime? createdAt;
+  String? name;
+  String? email;
+  String? password;
+  String? phoneNumber;
+  String? city;
+
+  Vendor({
+    this.id,
+    this.createdAt,
+    this.email,
+    this.password,
+    this.name,
+    this.phoneNumber,
+    this.city,
+  });
+
+  Vendor.fromJson(Map<String, dynamic> map)
+      : id = map['vendor_id'],
+        createdAt = DateTime.parse(map['created_at']),
+        email = map['vendor_email'],
+        password = map['vendor_password'],
+        name = map['vendor_name'],
+        phoneNumber = map['vendor_phone_number'],
+        city = map['vendor_city'];
+
+  Map<String, dynamic> toJson() => {
+        'vendor_id': id,
+        'created_at': createdAt?.toIso8601String(),
+        'vendor_email': email,
+        'vendor_password': password,
+        'vendor_name': name,
+        'vendor_phone_number': phoneNumber,
+        'vendor_city': city,
+      };
+}
