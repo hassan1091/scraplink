5a7d67c3b03aac752a42cd66dc8875f49fad28d0
حسن الخليف <roh.baba.11@gmail.com>
2023-11-21 21:57:08 +0300

    create material item card

---
+++ lib/widget/raw_material_item_card.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:scraplink/api/model/raw_material.dart';
+import 'package:scraplink/my_theme.dart';
+
+class RawMaterialItemCard extends StatelessWidget {
+  const RawMaterialItemCard({
+    super.key,
+    required this.rawMaterial,
+  });
+
+  final RawMaterial rawMaterial;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      elevation: 5,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          ClipRRect(
+            borderRadius:
+                const BorderRadius.vertical(top: Radius.circular(16.0)),
+            child: Image.network(
+              "https://xpfrdvwfquqhzzijsboi.supabase.co/storage/v1/object/public/raw_material/${rawMaterial.type}.png",
+              height: 100,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  rawMaterial.type!,
+                  style: MyTheme().titleStyle,
+                ),
+                Text(
+                  rawMaterial.description!,
+                  style: MyTheme().subtitleStyle,
+                ),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Buy Now",
+                      style: MyTheme().buttonTextStyle,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
