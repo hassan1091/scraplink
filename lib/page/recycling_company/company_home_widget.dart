b311001f1a1a158a9864163fd5ebb8f4277d152b
حسن الخليف <roh.baba.11@gmail.com>
2023-11-21 20:50:12 +0300

    init company home widget

---
+++ lib/page/recycling_company/company_home_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CompanyHomeWidget extends StatelessWidget {
+  const CompanyHomeWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+      child: Column(children: []),
+    );
+  }
+}
