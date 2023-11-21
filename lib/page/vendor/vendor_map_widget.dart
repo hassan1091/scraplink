be8ddc0daf3858f50237b914ab83328ce1fe32b4
حسن الخليف <roh.baba.11@gmail.com>
2023-11-21 18:32:37 +0300

    create vendor map widget and connect it to api

---
+++ lib/page/vendor/vendor_map_widget.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:scraplink/api/api_service.dart';
+import 'package:scraplink/api/model/user_profile.dart';
+import 'package:scraplink/constants.dart';
+import 'package:scraplink/my_theme.dart';
+import 'package:scraplink/widget/yard_card.dart';
+
+class VendorMapWidget extends StatefulWidget {
+  const VendorMapWidget({super.key});
+
+  @override
+  State<VendorMapWidget> createState() => _VendorMapWidgetState();
+}
+
+class _VendorMapWidgetState extends State<VendorMapWidget> {
+  String? selectedLocation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              "Location:",
+              style: MyTheme().titleStyle,
+            ),
+            DropdownButton(
+              value: selectedLocation,
+              hint: Text(
+                "Select a Location",
+                style: MyTheme().subtitleStyle,
+              ),
+              items: Constants.regions.map((String item) {
+                return DropdownMenuItem(
+                  value: item,
+                  child: Text(
+                    item,
+                    style: MyTheme().subtitleStyle,
+                  ),
+                );
+              }).toList(),
+              onChanged: (s) {
+                setState(() {
+                  selectedLocation = s;
+                });
+              },
+              icon: const Icon(Icons.arrow_drop_down),
+            ),
+          ],
+        ),
+        FutureBuilder(
+          future: ApiService().getRecycleCompanies(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
+            List<UserProfile> companies = snapshot.data!;
+            if (selectedLocation != null) {
+              companies = companies
+                  .where((element) => element.location == selectedLocation)
+                  .toList();
+            }
+            return Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemCount: companies.length,
+                itemBuilder: (context, index) =>
+                    YardCard(vendor: companies[index]),
+              ),
+            );
+          },
+        )
+      ],
+    );
+  }
+}
