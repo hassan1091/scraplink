0c9e608aa585895e2d099b2d71e11cf1f4213a40
حسن الخليف <roh.baba.11@gmail.com>
2023-11-21 19:09:49 +0300

    create item car card

---
+++ lib/widget/car_item_card.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:scraplink/api/model/car.dart';
+import 'package:scraplink/my_theme.dart';
+
+class CarItemCard extends StatelessWidget {
+  const CarItemCard({
+    super.key,
+    required this.car,
+  });
+
+  final Car car;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      elevation: 5,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          ClipRRect(
+            borderRadius:
+                const BorderRadius.vertical(top: Radius.circular(16.0)),
+            child: Image.network(
+              car.imageUrl!,
+              height: 100,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "${car.name} ${car.year}",
+                  style: MyTheme().titleStyle,
+                ),
+                Text(
+                  car.description!,
+                  style: MyTheme().subtitleStyle,
+                ),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Buy Now",
+                      style: MyTheme().buttonTextStyle,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
