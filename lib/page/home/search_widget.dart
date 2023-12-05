ce947af5b1c28273e22f460a0206e70359e4ab30
حسن الخليف <roh.baba.11@gmail.com>
2023-12-05 11:53:37 +0300

    add validation to search part

--- lib/page/home/search_widget.dart
+++ lib/page/home/search_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scraplink/constants.dart';
+import 'package:scraplink/field_validation.dart';
 import 'package:scraplink/my_theme.dart';
 import 'package:scraplink/page/home/available_part.dart';
 import 'package:scraplink/widget/my_dropdown_button.dart';
@@ -17,72 +18,80 @@ class SearchState extends State<SearchWidget> {
   String? _selectedMake;
   String? _selectedModel;
   String? _selectedCategory;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
-          MyDropdownButton(
-            label: "Make:",
-            hint: "Select a Make",
-            items: Constants.models.keys.toList(),
-            selectedItem: _selectedMake,
-            onChanged: (s) {
-              setState(() {
-                _selectedMake = s;
-              });
-            },
-          ),
-          const SizedBox(height: 16),
-          MyDropdownButton(
-            label: "Model:",
-            hint: "Select a Model",
-            items: Constants.models[_selectedMake] ?? [],
-            selectedItem: _selectedModel,
-            onChanged: (s) {
-              setState(() {
-                _selectedModel = s;
-              });
-            },
-          ),
-          const SizedBox(height: 16),
-          MyDropdownButton(
-            label: "Category:",
-            hint: "Select a Category",
-            items: Constants.partCategory,
-            selectedItem: _selectedCategory,
-            onChanged: (s) {
-              setState(() {
-                _selectedCategory = s;
-              });
-            },
-          ),
-          const SizedBox(height: 4),
-          MyTextFormField(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            MyDropdownButton(
+              label: "Make:",
+              hint: "Select a Make",
+              items: Constants.models.keys.toList(),
+              selectedItem: _selectedMake,
+              onChanged: (s) {
+                setState(() {
+                  _selectedMake = s;
+                });
+              },
+            ),
+            const SizedBox(height: 16),
+            MyDropdownButton(
+              label: "Model:",
+              hint: "Select a Model",
+              items: Constants.models[_selectedMake] ?? [],
+              selectedItem: _selectedModel,
+              onChanged: (s) {
+                setState(() {
+                  _selectedModel = s;
+                });
+              },
+            ),
+            const SizedBox(height: 16),
+            MyDropdownButton(
+              label: "Category:",
+              hint: "Select a Category",
+              items: Constants.partCategory,
+              selectedItem: _selectedCategory,
+              onChanged: (s) {
+                setState(() {
+                  _selectedCategory = s;
+                });
+              },
+            ),
+            const SizedBox(height: 4),
+            MyTextFormField(
               controller: _yearController,
               hint: "Year",
               lable: "Year",
-              type: const TextInputType.numberWithOptions()),
-          const SizedBox(height: 4),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => AvailablePartPage(
-                              make: _selectedMake,
-                              model: _selectedModel,
-                              category: _selectedCategory,
-                              year: _yearController.text,
-                            )));
-              },
-              child: Text(
-                "Search",
-                style: MyTheme().buttonTextStyle,
-              ))
-        ],
+              type: const TextInputType.numberWithOptions(),
+              validator: FieldValidation.validateYear,
+            ),
+            const SizedBox(height: 4),
+            ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AvailablePartPage(
+                                  make: _selectedMake,
+                                  model: _selectedModel,
+                                  category: _selectedCategory,
+                                  year: _yearController.text,
+                                )));
+                  }
+                },
+                child: Text(
+                  "Search",
+                  style: MyTheme().buttonTextStyle,
+                ))
+          ],
+        ),
       ),
     );
   }
