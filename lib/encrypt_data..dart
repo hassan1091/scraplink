b733211e24818c0b8569ea02d2f82ea86b530528
حسن الخليف <roh.baba.11@gmail.com>
2023-12-05 22:24:33 +0300

    create encrypt data class

---
+++ lib/encrypt_data..dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:encrypt/encrypt.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class EncryptData {
+  final encrypter = Encrypter(AES(generateKey()));
+  final iv = IV.fromUtf8(dotenv.env['SECRET_KEY']!);
+
+  String encryptData(String data) {
+    return encrypter.encrypt(data, iv: iv).base64;
+  }
+
+  String decryptData(String encryptedData) {
+    return encrypter.decrypt64(encryptedData, iv: iv);
+  }
+
+  static Key generateKey() {
+    final keyString = dotenv.env['SECRET_KEY']!;
+    return Key(Uint8List.fromList(base64Url.decode(keyString)));
+  }
+}
